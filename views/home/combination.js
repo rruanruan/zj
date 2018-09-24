5c1158c4375f9379e01473b224752e7771ec182c
何坦宏 <hetanhong2015@ipalfish.com>
2018-09-24 10:23:37 +0800

    sentry

--- views/home/combination.js
+++ views/home/combination.js
@@ -8,7 +8,7 @@ import Raven from 'raven-js';
 import RavenVue from 'raven-js/plugins/vue';
 console.log(process.env);
 
-Raven.config('http://1c83977e7d9c4d45b0a6aca6bce0be49@hetanhong.sentry.com/2', {
+Raven.config('http://1c83977e7d9c4d45b0a6aca6bce0be49@192.168.1.102:9000/2', {
     release: process.env.RELEASE_VERSION,
     headers: {
         'x-sentry-token': process.env.X_SENTRY_TOKEN
