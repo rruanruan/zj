352569ad51ab1a305cf369b356570be36e12de28
何坦宏 <hetanhong2015@ipalfish.com>
2018-09-27 10:52:29 +0800

    sentry test

--- views/home/combination.js
+++ views/home/combination.js
@@ -21,12 +21,43 @@ Vue.config.errorHandler = function VueErrorHandler(error) {
         extra: metaData
     });
 };
+
 Raven.config('http://1c83977e7d9c4d45b0a6aca6bce0be49@192.168.0.105:9000/2', {
     release: process.env.RELEASE_VERSION
-})
-    .addPlugin(RavenVue, Vue)
-    .install();
+});
+Vue.config.errorHandler = function VueErrorHandler(error, vm, info) {
+    let metaData = {
+        phone: function() {
+            return Math.random()
+                .toString()
+                .substr(2, 11);
+        },
+        uid: 'hello'
+    };
 
+    // vm and lifecycleHook are not always available
+    if (Object.prototype.toString.call(vm) === '[object Object]') {
+        metaData.componentName = formatComponentName(vm);
+        metaData.propsData = vm.$options.propsData;
+    }
+
+    if (typeof info !== 'undefined') {
+        metaData.lifecycleHook = info;
+    }
+
+    Raven.captureException(error, {
+        extra: metaData
+    });
+};
+function formatComponentName(vm) {
+    if (vm.$root === vm) {
+        return 'root instance';
+    }
+    let name = vm._isVue ? vm.$options.name || vm.$options._componentTag : vm.name;
+    return (
+        (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options.__file ? ' at ' + vm.$options.__file : '')
+    );
+}
 new Vue({
     el: '#container',
     render: h => h(Container)
